d15e283016587996cd3694a70e9169119072e8cb
Banana Person <ilikebananas@callme.com>
2016-08-01 10:10:33 +0300

    test

--- database_setup.py
+++ database_setup.py
@@ -2,8 +2,32 @@ from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.dialects import postgresql
 
 Base = declarative_base()
 
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(60))
+    username = Column(String(60))
+    password = Column(String(60))
+    gender = Column(String(30))
+    nationality = Column(String(30))
+    bio = Column(String(300))
+    rating = Column(Integer)
+
+
+class Event(Base):
+    __tablename__ = 'event'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(60))
+    date = Column(Date(60))
+    chef = Column(Integer, ForeignKey("person.id"))
+    attending = Column
+    nationality = Column(String(30))
+    bio = Column(String(300))
+    rating = Column(Integer)
+    
 #PLACE YOUR TABLE SETUP INFORMATION HERE
 
